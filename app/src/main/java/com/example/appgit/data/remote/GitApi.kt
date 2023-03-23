3acfaf6c537aa88ebba06c335fad3f0bfd584989
dauni <daunitisivan.999@mail.ru>
2023-03-23 21:40:51 +0300

    create impl repository in data layer from domain layer

--- app/src/main/java/com/example/appgit/data/remote/GitApi.kt
+++ app/src/main/java/com/example/appgit/data/remote/GitApi.kt
@@ -3,6 +3,5 @@ package com.example.appgit.data.remote
 import com.example.appgit.data.model.Commit
 
 interface GitApi {
-
     suspend fun getAllCommit() : List<Commit>
 }
--- app/src/main/java/com/example/appgit/data/repository/RepositoryGitImpl.kt
+++ app/src/main/java/com/example/appgit/data/repository/RepositoryGitImpl.kt
@@ -1,9 +1,14 @@
 package com.example.appgit.data.repository
 
+import com.example.appgit.data.mapper.toCommitDomain
 import com.example.appgit.data.remote.GitApi
+import com.example.appgit.domain.model.CommitDomain
+import com.example.appgit.domain.repository.RepositoryGit
 
 class RepositoryGitImpl(
     private val api: GitApi
-) {
-
+) : RepositoryGit {
+    override suspend fun getAllCommit(): List<CommitDomain> {
+        return api.getAllCommit().map { it.toCommitDomain() }
+    }
 }
