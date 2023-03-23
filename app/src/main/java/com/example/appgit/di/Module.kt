952ada728eb641d4b546b957856992bf2b00b707
dauni <daunitisivan.999@mail.ru>
2023-03-23 22:13:05 +0300

    last fix function method in Module

--- app/src/main/java/com/example/appgit/di/Module.kt
+++ app/src/main/java/com/example/appgit/di/Module.kt
@@ -3,7 +3,7 @@ package com.example.appgit.di
 import com.example.appgit.data.repository.RepositoryGitImpl
 import com.example.appgit.domain.repository.RepositoryGit
 
-abstract class Module {
+interface Module {
 
-    abstract fun bindRepositoryGit(impl: RepositoryGitImpl): RepositoryGit
+    fun bindRepositoryGit(impl: RepositoryGitImpl): RepositoryGit
 }
